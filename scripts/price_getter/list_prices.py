57d558b2641ab0cc2e4da9a2d60b807af3caa809
Alan Smith <commit@alanwsmith.com>
2022-09-29 12:51:40 -0400

    Updated formatting

--- scripts/price_getter/list_prices.py
+++ scripts/price_getter/list_prices.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
 import json
+import re
 
 from string import Template
 
+# Yeah, you're right. This code ain't pretty,
+# but it's one off code and it got the job
+# done.
 
 with open('raw_price_data.json') as _json:
     raw_data = json.load(_json)
 
 products = []
 
+# Post filter here. 
 for item in raw_data:
     if 'instanceType' in item['product']['attributes']:
         if item['product']['attributes']['tenancy'] == 'Shared':
@@ -17,34 +22,24 @@ for item in raw_data:
                 if item['product']['attributes']['capacitystatus'] == 'Used':
                     if item['product']['attributes']['preInstalledSw'] == 'NA':
                         item['cost'] = round(float(list(list(item['terms']['OnDemand'].items())[0][1]['priceDimensions'].items())[0][1]['pricePerUnit']['USD']), 3)
-                #if item['product']['attributes']['instanceFamily'] == 'GPU instance':
                         products.append(item)
 
-        # print(item['product']['attributes']['instance'])
-        #if 'instanceType' not in item['product']['attributes']:
-            #print(item)
-
 skeleton = Template("""<!DOCTYPE html>
-<html>
-<head>
-<style>
+<html><head><style>
 body { background-color: #222; color: #588; }
-td { vertical-align: top; }
+td { vertical-align: top; padding-right: 8px; }
+.name_1, .name_2, .vcpu, .gpu, .memory { text-align: center;}
 </style>
-<body>
-<table><tbody>
-<tr>
-<th>Category</th>
-<th>Name</th>
+<body><table><tbody><tr>
+<th>Family</th>
+<th colspan="2">Name</th>
 <th>vCPUs</th>
 <th>GPUs</th>
 <th>Memory</th>
 <th>Cost/Hour</th>
 </tr>
 $TABLE_ROWS
-</tbody></table>
-</body>
-</html>
+</tbody></table></body></html>
 """)
 
 table_rows = ''
@@ -59,10 +54,44 @@ for item in sorted(
             x['product']['attributes']['instanceType'])
         ):
 
+    matches = re.search(
+        r"(\w+)(\d+)(.*)", 
+        item['product']['attributes']['instanceType'].split('.')[0]
+    )
+    item['name_1'] = matches.group(1)
+    item['name_2'] = matches.group(2)
+    item['name_3'] = matches.group(3)
+
+    item['name_key'] = item['product']['attributes']['instanceType'].split('.')[0]
+
+    gpu_names = {
+        'p4d': 'NVIDIA A100 Tensor Core',
+        'p4de': 'NVIDIA A100 Tensor Core',
+        'p3': 'NVIDIA Tesla V100',
+        'p3dn': 'NVIDIA Tesla V100',
+        'p2': 'NVIDIA K80',
+        'g5': 'NVIDIA A10G Tensor Core',
+        'g5g': 'NVIDIA T4G Tensor Core',
+        'g4dn': 'NVIDIA T4 Tensor Core',
+        'g4ad': 'AMD Radeon Pro V520',
+        'g3': 'NVIDIA Tesla M60',
+        'g3s': 'NVIDIA Tesla M60',
+        'g2': '(Not listed)'
+
+    }
+
+    if item['name_key'] in gpu_names:
+        item['gpu_name'] = gpu_names[item['name_key']]
+    else:
+        item['gpu_name'] = ''
+
     if 'gpu' in item['product']['attributes']:
         item['gpu'] = item['product']['attributes']['gpu']
     else:
         item['gpu'] = '0'
+
+    item['family'] = item['product']['attributes']['instanceFamily'].replace(' Instances', '').replace('Machine Learning', 'ML').replace('optimized', '').replace('instance', '').replace('purpose', '').replace('Accelerator', '')
+
 
 
     # NOTE: this assumes there's only one item in the OnDemand
@@ -70,17 +99,16 @@ for item in sorted(
     # if you give it a try. 
     table_rows += f"""
 <tr>
-<td>{item['product']['attributes']['instanceFamily'].replace(' Instances', '').replace('Machine Learning', 'ML')}</td>
-<td>{item['product']['attributes']['instanceType']}</td>
-<td>{item['product']['attributes']['vcpu']}</td>
-<td>{item['product']['attributes']['memory'].replace(' GiB', '')}</td>
-<td>{item['gpu']}</td>
-<td>${item['cost']}</td>
+<td class="family">{item['family']}</td>
+<td class="name_1">{item['product']['attributes']['instanceType'].split('.')[0]}</td>
+<td class="name_2">{item['product']['attributes']['instanceType'].split('.')[1]}</td>
+<td class="vcpu">{item['product']['attributes']['vcpu']}</td>
+<td class="gpu">{item['gpu']}</td>
+<td class="memory">{item['product']['attributes']['memory'].replace(' GiB', '')}</td>
+<td class="cost">${item['cost']}</td>
+<td class="gpu_name">{item['gpu_name']}</td>
 </tr>
 """
-
-# <td><pre><code>{json.dumps(item, sort_keys=True, indent=2)}</td>
-# <td>{item}</td>
 
 with open('prices.html', 'w') as _out:
     _out.write(skeleton.substitute(TABLE_ROWS=table_rows))
