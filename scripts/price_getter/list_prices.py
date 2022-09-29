e0928ec8ae2c7bb678e501a6d50d5a9d0e2b5c5b
Alan Smith <commit@alanwsmith.com>
2022-09-29 01:46:08 -0400

    Prices in place

--- scripts/price_getter/list_prices.py
+++ scripts/price_getter/list_prices.py
@@ -16,7 +16,7 @@ for item in raw_data:
             if item['product']['attributes']['marketoption'] == 'OnDemand':
                 if item['product']['attributes']['capacitystatus'] == 'Used':
                     if item['product']['attributes']['preInstalledSw'] == 'NA':
-                        item['cost'] = list(list(item['terms']['OnDemand'].items())[0][1]['priceDimensions'].items())[0][1]['pricePerUnit']['USD']
+                        item['cost'] = round(float(list(list(item['terms']['OnDemand'].items())[0][1]['priceDimensions'].items())[0][1]['pricePerUnit']['USD']), 3)
                 #if item['product']['attributes']['instanceFamily'] == 'GPU instance':
                         products.append(item)
 
@@ -33,6 +33,14 @@ td { vertical-align: top; }
 </style>
 <body>
 <table><tbody>
+<tr>
+<th>Category</th>
+<th>Name</th>
+<th>vCPUs</th>
+<th>GPUs</th>
+<th>Memory</th>
+<th>Cost/Hour</th>
+</tr>
 $TABLE_ROWS
 </tbody></table>
 </body>
@@ -45,20 +53,29 @@ for item in sorted(
         key=lambda 
         x: (
             x['product']['attributes']['instanceFamily'], 
+            x['cost'], 
             x['product']['attributes']['instanceType'].split('.')[0],
             int(x['product']['attributes']['vcpu']),
             x['product']['attributes']['instanceType'])
         ):
+
+    if 'gpu' in item['product']['attributes']:
+        item['gpu'] = item['product']['attributes']['gpu']
+    else:
+        item['gpu'] = '0'
+
+
     # NOTE: this assumes there's only one item in the OnDemand
     # terms - that's true for my use case, but check your math
     # if you give it a try. 
     table_rows += f"""
 <tr>
-<td>{item['product']['attributes']['instanceFamily']}</td>
+<td>{item['product']['attributes']['instanceFamily'].replace(' Instances', '').replace('Machine Learning', 'ML')}</td>
 <td>{item['product']['attributes']['instanceType']}</td>
 <td>{item['product']['attributes']['vcpu']}</td>
-<td>{item['cost']}</td>
-<td>{list(list(item['terms']['OnDemand'].items())[0][1]['priceDimensions'].items())[0][1]['pricePerUnit']['USD']}</td>
+<td>{item['product']['attributes']['memory'].replace(' GiB', '')}</td>
+<td>{item['gpu']}</td>
+<td>${item['cost']}</td>
 </tr>
 """
 
