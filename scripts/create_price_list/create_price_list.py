0d0ea992455c39740b5dc020cffd8031954a4ff5
Alan Smith <commit@alanwsmith.com>
2022-09-30 19:45:40 -0400

    Removed comments

--- scripts/create_price_list/create_price_list.py
+++ scripts/create_price_list/create_price_list.py
@@ -46,24 +46,6 @@ for item in raw_data:
                             item['gpu'] = '0'
                         products.append(item)
 
-# skeleton = Template("""<!DOCTYPE html>
-# <html><head><style>
-# body { background-color: #222; color: #588; }
-# td { vertical-align: top; padding-right: 8px; }
-# .name_1, .name_2, .vcpu, .gpu, .memory { text-align: center;}
-# </style>
-# <body><table><tbody><tr>
-# <th>Family</th>
-# <th colspan="2">Name</th>
-# <th>vCPUs</th>
-# <th>GPUs</th>
-# <th>Memory</th>
-# <th>Cost/Hour</th>
-# </tr>
-# $TABLE_ROWS
-# </tbody></table></body></html>
-# """)
-
 table_rows = ''
 for item in sorted(
         products, 
@@ -85,34 +67,6 @@ for item in sorted(
     item['name_2'] = matches.group(2)
     item['name_3'] = matches.group(3)
 
-    # item['name_key'] = item['product']['attributes']['instanceType'].split('.')[0]
-
-    # gpu_names = {
-    #     'p4d': 'NVIDIA A100 Tensor Core',
-    #     'p4de': 'NVIDIA A100 Tensor Core',
-    #     'p3': 'NVIDIA Tesla V100',
-    #     'p3dn': 'NVIDIA Tesla V100',
-    #     'p2': 'NVIDIA K80',
-    #     'g5': 'NVIDIA A10G Tensor Core',
-    #     'g5g': 'NVIDIA T4G Tensor Core',
-    #     'g4dn': 'NVIDIA T4 Tensor Core',
-    #     'g4ad': 'AMD Radeon Pro V520',
-    #     'g3': 'NVIDIA Tesla M60',
-    #     'g3s': 'NVIDIA Tesla M60',
-    #     'g2': '(Not listed)'
-    # }
-
-    # if item['name_key'] in gpu_names:
-    #     item['gpu_name'] = gpu_names[item['name_key']]
-    # else:
-    #     item['gpu_name'] = ''
-    # if 'gpu' in item['product']['attributes']:
-    #     item['gpu'] = item['product']['attributes']['gpu']
-    # else:
-    #     item['gpu'] = '0'
-
-
-    # item['family_with_gpu'] = item['family_with_gpu'].replace(' Instances', '').replace('Machine Learning', 'ML').replace('optimized', '').replace('instance', '').replace('purpose', '').replace('Accelerator', '')
     item['family_with_gpu'] = item['family_with_gpu'].replace(' instance', '').replace(' Instances', '')
 
     # NOTE: this assumes there's only one item in the OnDemand
